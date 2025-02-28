629996957ee9da9bda9797539fd8a9c8ad990497
GabrielSantiago <marcosgabrielss23@gmail.com>
2025-02-27 21:49:12 -0300

    creating a fake products repository

--- ecommerce_app/lib/src/features/products/data/products_fake_repository.dart
+++ ecommerce_app/lib/src/features/products/data/products_fake_repository.dart
@@ -1,7 +1,22 @@
+import 'package:ecommerce_app/src/constants/test_products.dart';
 import 'package:ecommerce_app/src/features/products/domain/product.dart';
 
 class ProductsFakeRepository {
+  ProductsFakeRepository._();
+
+  static ProductsFakeRepository instance = ProductsFakeRepository._();
+
+  final List<Product> _products = kTestProducts;
+
   List<Product> getProducts() {
-    return [];
+    return _products;
+  }
+
+  Product? getProductById(String id) {
+    try {
+      return _products.firstWhere((element) => element.id == id);
+    } catch (e) {
+      return null;
+    }
   }
 }
