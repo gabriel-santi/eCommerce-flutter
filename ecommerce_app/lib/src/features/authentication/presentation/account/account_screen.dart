7e2c647efa024c249c43c16dde8d155e62b98e6a
GabrielSantiago <marcosgabrielss23@gmail.com>
2025-05-25 11:48:41 -0300

    Consuming auth repository

--- ecommerce_app/lib/src/features/authentication/presentation/account/account_screen.dart
+++ ecommerce_app/lib/src/features/authentication/presentation/account/account_screen.dart
@@ -2,7 +2,6 @@ import 'package:ecommerce_app/src/common_widgets/alert_dialogs.dart';
 import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
 import 'package:ecommerce_app/src/features/authentication/presentation/account/account_screen_controller.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
-import 'package:ecommerce_app/src/features/authentication/domain/app_user.dart';
 import 'package:ecommerce_app/src/utils/async_value_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:ecommerce_app/src/common_widgets/action_text_button.dart';
@@ -29,9 +28,6 @@ class AccountScreen extends ConsumerWidget {
             onPressed: state.isLoading
                 ? null
                 : () async {
-                    // * Get the navigator beforehand to prevent this warning:
-                    // * Don't use 'BuildContext's across async gaps.
-                    // * More info here: https://youtu.be/bzWaMpD1LHY
                     final goRouter = GoRouter.of(context);
                     final logout = await showAlertDialog(
                       context: context,
@@ -58,14 +54,14 @@ class AccountScreen extends ConsumerWidget {
 }
 
 /// Simple user data table showing the uid and email
-class UserDataTable extends StatelessWidget {
+class UserDataTable extends ConsumerWidget {
   const UserDataTable({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final style = Theme.of(context).textTheme.titleSmall!;
-    // TODO: get user from auth repository
-    const user = AppUser(uid: '123', email: 'test@test.com');
+    final user = ref.watch(authRepositoryProvider).currentUser;
+
     return DataTable(
       columns: [
         DataColumn(
@@ -84,12 +80,12 @@ class UserDataTable extends StatelessWidget {
       rows: [
         _makeDataRow(
           'uid'.hardcoded,
-          user.uid,
+          user?.uid ?? '',
           style,
         ),
         _makeDataRow(
           'email'.hardcoded,
-          user.email ?? '',
+          user?.email ?? '',
           style,
         ),
       ],
