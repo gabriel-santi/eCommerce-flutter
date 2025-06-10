17f4e64fde7a4f3427929769c9bfba2ac37d30d8
GabrielSantiago <marcosgabrielss23@gmail.com>
2025-06-09 22:42:47 -0300

    Tests for account screen controller

--- ecommerce_app/lib/src/features/authentication/presentation/account/account_screen_controller.dart
+++ ecommerce_app/lib/src/features/authentication/presentation/account/account_screen_controller.dart
@@ -2,7 +2,7 @@ import 'package:ecommerce_app/src/features/authentication/data/auth_repository.d
 import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class AccountScreenController extends StateNotifier<AsyncValue> {
+class AccountScreenController extends StateNotifier<AsyncValue<void>> {
   AccountScreenController({required this.authRepository})
       : super(const AsyncValue.data(null));
 
---
+++ ecommerce_app/test/src/features/authentication/presentation/account/account_screen_controller_test.dart
@@ -0,0 +1,44 @@
+import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
+import 'package:ecommerce_app/src/features/authentication/presentation/account/account_screen_controller.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockAuthRepository extends Mock implements FakeAuthRepository {}
+
+void main() {
+  group("AccountScreenController", () {
+    test("Initial state value shoud be null", () {
+      final authRepo = MockAuthRepository();
+      final controller = AccountScreenController(authRepository: authRepo);
+      verifyNever(authRepo.signOut);
+      expect(controller.debugState, const AsyncData<void>(null));
+    });
+
+    test("SignOut successfuly", () async {
+      //setup
+      final authRepo = MockAuthRepository();
+      when(authRepo.signOut).thenAnswer((invocation) => Future.value());
+      final controller = AccountScreenController(authRepository: authRepo);
+      // run
+      await controller.signOut();
+      //verify
+      verify(authRepo.signOut).called(1);
+      expect(controller.debugState, AsyncData<void>(null));
+    });
+
+    test("SignOut failure", () async {
+      //setup
+      final authRepo = MockAuthRepository();
+      final exception = Exception("Connection failed");
+      when(authRepo.signOut).thenThrow(exception);
+      final controller = AccountScreenController(authRepository: authRepo);
+      // run
+      await controller.signOut();
+      //verify
+      verify(authRepo.signOut).called(1);
+      expect(controller.debugState.hasError, true);
+      expect(controller.debugState, isA<AsyncError>());
+    });
+  });
+}
