63f3c265edcb58257ffb2e581fa13369c2662c43
GabrielSantiago <marcosgabrielss23@gmail.com>
2025-06-17 00:13:31 -0300

    Test widgets on signout success

--- ecommerce_app/test/src/features/authentication/auth_robot.dart
+++ ecommerce_app/test/src/features/authentication/auth_robot.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_app/src/common_widgets/alert_dialogs.dart';
 import 'package:ecommerce_app/src/features/authentication/presentation/account/account_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -10,7 +11,7 @@ class AuthRobot {
 
   Future<void> pumpAccountScreen() async {
     await tester.pumpWidget(
-      ProviderScope(
+      const ProviderScope(
         child: MaterialApp(
           home: AccountScreen(),
         ),
@@ -42,5 +43,13 @@ class AuthRobot {
   void expectLogoutDialogNotFound() {
     final dialogTitle = find.text('Are you sure?');
     expect(dialogTitle, findsNothing);
+  }
+
+  Future<void> tapDialogLogoutButton() async {
+    final logoutButton = find.byKey(kDialogDefaultKey);
+    expect(logoutButton, findsOneWidget);
+
+    await tester.tap(logoutButton);
+    await tester.pump();
   }
 }
