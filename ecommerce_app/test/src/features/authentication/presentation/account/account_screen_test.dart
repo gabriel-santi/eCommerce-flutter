34a1f9ac7a422e8682e5040ed2d9684bdea194d2
GabrielSantiago <marcosgabrielss23@gmail.com>
2025-06-23 20:47:48 -0300

    Marking widgets tests with a tag

--- ecommerce_app/test/src/features/authentication/presentation/account/account_screen_test.dart
+++ ecommerce_app/test/src/features/authentication/presentation/account/account_screen_test.dart
@@ -5,52 +5,70 @@ import '../../../../mocks.dart';
 import '../../auth_robot.dart';
 
 void main() {
-  testWidgets('Cancel logout', (tester) async {
-    final r = AuthRobot(tester);
-    await r.pumpAccountScreen();
-    await r.tapLogoutButton();
-    r.expectLogoutDialogFound();
-    await r.tapCancelButton();
-    r.expectLogoutDialogNotFound();
-    r.expectErrorDialogNotFound();
-  });
+  testWidgets(
+    'Cancel logout',
+    (tester) async {
+      final r = AuthRobot(tester);
+      await r.pumpAccountScreen();
+      await r.tapLogoutButton();
+      r.expectLogoutDialogFound();
+      await r.tapCancelButton();
+      r.expectLogoutDialogNotFound();
+      r.expectErrorDialogNotFound();
+    },
+    tags: ['widgets'],
+  );
 
-  testWidgets('Confirm logout, success', (tester) async {
-    final r = AuthRobot(tester);
-    await r.pumpAccountScreen();
-    await r.tapLogoutButton();
-    r.expectLogoutDialogFound();
-    await r.tapDialogLogoutButton();
-    r.expectLogoutDialogNotFound();
-    r.expectErrorDialogNotFound();
-  });
+  testWidgets(
+    'Confirm logout, success',
+    (tester) async {
+      final r = AuthRobot(tester);
+      await r.pumpAccountScreen();
+      await r.tapLogoutButton();
+      r.expectLogoutDialogFound();
+      await r.tapDialogLogoutButton();
+      r.expectLogoutDialogNotFound();
+      r.expectErrorDialogNotFound();
+    },
+    tags: ['widgets'],
+  );
 
-  testWidgets('Confirm logout, failure', (tester) async {
-    final r = AuthRobot(tester);
-    final authRepository = MockAuthRepository();
-    final exception = Exception("Connection failed");
-    when(authRepository.signOut).thenThrow(exception);
-    when(authRepository.authStateChanges).thenAnswer((_) => Stream.value(null));
-    await r.pumpAccountScreen(authRepository: authRepository);
-    await r.tapLogoutButton();
-    r.expectLogoutDialogFound();
-    await r.tapDialogLogoutButton();
-    r.expectErrorDialogFound();
-  });
-
-  testWidgets('Confirm logout loading state', (tester) async {
-    final r = AuthRobot(tester);
-    final authRepository = MockAuthRepository();
-    when(authRepository.signOut)
-        .thenAnswer((_) => Future.delayed(const Duration(seconds: 1)));
-    when(authRepository.authStateChanges).thenAnswer((_) => Stream.value(null));
-    await tester.runAsync(() async {
+  testWidgets(
+    'Confirm logout, failure',
+    (tester) async {
+      final r = AuthRobot(tester);
+      final authRepository = MockAuthRepository();
+      final exception = Exception("Connection failed");
+      when(authRepository.signOut).thenThrow(exception);
+      when(authRepository.authStateChanges)
+          .thenAnswer((_) => Stream.value(null));
       await r.pumpAccountScreen(authRepository: authRepository);
       await r.tapLogoutButton();
       r.expectLogoutDialogFound();
       await r.tapDialogLogoutButton();
-    });
-    r.expectErrorDialogNotFound();
-    r.expectCircularProgressIndicator();
-  });
+      r.expectErrorDialogFound();
+    },
+    tags: ['widgets'],
+  );
+
+  testWidgets(
+    'Confirm logout loading state',
+    (tester) async {
+      final r = AuthRobot(tester);
+      final authRepository = MockAuthRepository();
+      when(authRepository.signOut)
+          .thenAnswer((_) => Future.delayed(const Duration(seconds: 1)));
+      when(authRepository.authStateChanges)
+          .thenAnswer((_) => Stream.value(null));
+      await tester.runAsync(() async {
+        await r.pumpAccountScreen(authRepository: authRepository);
+        await r.tapLogoutButton();
+        r.expectLogoutDialogFound();
+        await r.tapDialogLogoutButton();
+      });
+      r.expectErrorDialogNotFound();
+      r.expectCircularProgressIndicator();
+    },
+    tags: ['widgets'],
+  );
 }
--- ecommerce_app/test/src/features/authentication/presentation/sign_in/email_password_sign_in_screen_test.dart
+++ ecommerce_app/test/src/features/authentication/presentation/sign_in/email_password_sign_in_screen_test.dart
@@ -15,91 +15,107 @@ void main() {
   });
 
   group("Sign in ", () {
-    testWidgets('''
+    testWidgets(
+      '''
       Given formType is signIn
       When tap on the sign-in button
       Then signInWithEmailAndPassword is not called
-    ''', (tester) async {
-      final r = AuthRobot(tester);
-      await r.pumpEmailPasswordSignInContents(
-        authRepository: authRepository,
-        formType: EmailPasswordSignInFormType.signIn,
-      );
-      verifyNever(
-        () => authRepository.signInWithEmailAndPassword(any(), any()),
-      );
-    });
+    ''',
+      (tester) async {
+        final r = AuthRobot(tester);
+        await r.pumpEmailPasswordSignInContents(
+          authRepository: authRepository,
+          formType: EmailPasswordSignInFormType.signIn,
+        );
+        verifyNever(
+          () => authRepository.signInWithEmailAndPassword(any(), any()),
+        );
+      },
+      tags: ['widgets'],
+    );
 
-    testWidgets('''
+    testWidgets(
+      '''
       Given formType is signIn
       When enter valid email and password 
       And tap on the sign-in button
       Then onSignIn callback is called
       And error popup not found
-    ''', (tester) async {
-      bool didSignIn = false;
-      final r = AuthRobot(tester);
-      when(() => authRepository.signInWithEmailAndPassword(
-          testEmail, testPassword)).thenAnswer((_) => Future.value());
-      await r.pumpEmailPasswordSignInContents(
-          authRepository: authRepository,
-          formType: EmailPasswordSignInFormType.signIn,
-          onSignIn: () => didSignIn = true);
-      await r.enterEmail(testEmail);
-      await r.enterPassword(testPassword);
-      await r.tapEmailPasswordSubmitButton();
-      verify(
-        () =>
-            authRepository.signInWithEmailAndPassword(testEmail, testPassword),
-      ).called(1);
-      r.expectErrorDialogNotFound();
-      expect(didSignIn, true);
-    });
+    ''',
+      (tester) async {
+        bool didSignIn = false;
+        final r = AuthRobot(tester);
+        when(() => authRepository.signInWithEmailAndPassword(
+            testEmail, testPassword)).thenAnswer((_) => Future.value());
+        await r.pumpEmailPasswordSignInContents(
+            authRepository: authRepository,
+            formType: EmailPasswordSignInFormType.signIn,
+            onSignIn: () => didSignIn = true);
+        await r.enterEmail(testEmail);
+        await r.enterPassword(testPassword);
+        await r.tapEmailPasswordSubmitButton();
+        verify(
+          () => authRepository.signInWithEmailAndPassword(
+              testEmail, testPassword),
+        ).called(1);
+        r.expectErrorDialogNotFound();
+        expect(didSignIn, true);
+      },
+      tags: ['widgets'],
+    );
   });
 
   group("register", () {
-    testWidgets('''
+    testWidgets(
+      '''
       Given formType is register
       When tap on the submit button
       Then createUserWithEmailAndPassword is not called
-    ''', (tester) async {
-      final r = AuthRobot(tester);
-      await r.pumpEmailPasswordSignInContents(
-        authRepository: authRepository,
-        formType: EmailPasswordSignInFormType.signIn,
-      );
-      await r.tapEmailPasswordSubmitButton();
-      verifyNever(
-        () => authRepository.createUserWithEmailAndPassword(any(), any()),
-      );
-    });
+    ''',
+      (tester) async {
+        final r = AuthRobot(tester);
+        await r.pumpEmailPasswordSignInContents(
+          authRepository: authRepository,
+          formType: EmailPasswordSignInFormType.signIn,
+        );
+        await r.tapEmailPasswordSubmitButton();
+        verifyNever(
+          () => authRepository.createUserWithEmailAndPassword(any(), any()),
+        );
+      },
+      tags: ['widgets'],
+    );
 
-    testWidgets('''
+    testWidgets(
+      '''
       Given formType is register
       When enter valid email and password 
       And tap on the submit button
       Then createUserWithEmailAndPassword is called
       And signInWithEmailAndPassword is not called
       And error popup not found
-    ''', (tester) async {
-      final r = AuthRobot(tester);
-      when(() => authRepository.createUserWithEmailAndPassword(
-          testEmail, testPassword)).thenAnswer((_) => Future.value());
-      await r.pumpEmailPasswordSignInContents(
-        authRepository: authRepository,
-        formType: EmailPasswordSignInFormType.register,
-      );
-      await r.enterEmail(testEmail);
-      await r.enterPassword(testPassword);
-      await r.tapEmailPasswordSubmitButton();
-      verify(
-        () => authRepository.createUserWithEmailAndPassword(
-            testEmail, testPassword),
-      ).called(1);
-      verifyNever(
-        () => authRepository.createUserWithEmailAndPassword(any(), any()),
-      );
-      r.expectErrorDialogNotFound();
-    });
+    ''',
+      (tester) async {
+        final r = AuthRobot(tester);
+        when(() => authRepository.createUserWithEmailAndPassword(
+            testEmail, testPassword)).thenAnswer((_) => Future.value());
+        await r.pumpEmailPasswordSignInContents(
+          authRepository: authRepository,
+          formType: EmailPasswordSignInFormType.register,
+        );
+        await r.enterEmail(testEmail);
+        await r.enterPassword(testPassword);
+        await r.tapEmailPasswordSubmitButton();
+        verify(
+          () => authRepository.createUserWithEmailAndPassword(
+              testEmail, testPassword),
+        ).called(1);
+        verifyNever(
+          () => authRepository.createUserWithEmailAndPassword(any(), any()),
+        );
+        r.expectErrorDialogNotFound();
+      },
+      tags: ['widgets'],
+    );
   });
 }
