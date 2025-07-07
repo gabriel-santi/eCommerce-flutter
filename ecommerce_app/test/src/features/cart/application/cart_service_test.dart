2c25cb684ca0e5076eb669da5953bf21893b69d8
GabrielSantiago <marcosgabrielss23@gmail.com>
2025-07-07 17:12:50 -0300

    Tests for total price provider

--- ecommerce_app/test/src/features/cart/application/cart_service_test.dart
+++ ecommerce_app/test/src/features/cart/application/cart_service_test.dart
@@ -38,15 +38,13 @@ void main() {
     return container.read(cartServiceProvider);
   }
 
-  group("setItem", () {
+  group('setItem', () {
     test("when null user, writes item to local cart", () async {
       // setup
       const expectedCart = Cart({'123': 1});
       when(() => authRepository.currentUser).thenReturn(null);
-      when(localCartRepository.fetchCart)
-          .thenAnswer((_) => Future.value(const Cart()));
-      when(() => localCartRepository.setCart(expectedCart))
-          .thenAnswer((_) => Future.value());
+      when(localCartRepository.fetchCart).thenAnswer((_) => Future.value(const Cart()));
+      when(() => localCartRepository.setCart(expectedCart)).thenAnswer((_) => Future.value());
       final cartService = makeCartService();
       // run
       await cartService.setItem(Item(productId: '123', quantity: 1));
@@ -59,16 +57,13 @@ void main() {
       // setup
       final expectedCart = Cart({'123': 1});
       when(() => authRepository.currentUser).thenReturn(testUser);
-      when(() => remoteCartRepository.fetchCart(testUser.uid))
-          .thenAnswer((_) => Future.value(const Cart()));
-      when(() => remoteCartRepository.setCart(testUser.uid, expectedCart))
-          .thenAnswer((_) => Future.value());
+      when(() => remoteCartRepository.fetchCart(testUser.uid)).thenAnswer((_) => Future.value(const Cart()));
+      when(() => remoteCartRepository.setCart(testUser.uid, expectedCart)).thenAnswer((_) => Future.value());
       final cartService = makeCartService();
       // run
       await cartService.setItem(const Item(productId: '123', quantity: 1));
       // verify
-      verify(() => remoteCartRepository.setCart(testUser.uid, expectedCart))
-          .called(1);
+      verify(() => remoteCartRepository.setCart(testUser.uid, expectedCart)).called(1);
       verifyNever(() => localCartRepository.setCart(any()));
     });
   });
@@ -79,10 +74,8 @@ void main() {
       const initialCart = Cart({'123': 2});
       const expectedCart = Cart({'123': 3});
       when(() => authRepository.currentUser).thenReturn(null); // null user
-      when(localCartRepository.fetchCart)
-          .thenAnswer((_) => Future.value(initialCart));
-      when(() => localCartRepository.setCart(expectedCart))
-          .thenAnswer((_) => Future.value());
+      when(localCartRepository.fetchCart).thenAnswer((_) => Future.value(initialCart));
+      when(() => localCartRepository.setCart(expectedCart)).thenAnswer((_) => Future.value());
       final cartService = makeCartService();
       // run
       await cartService.addItem(const Item(productId: '123', quantity: 1));
@@ -96,17 +89,14 @@ void main() {
       const initialCart = Cart({'123': 2});
       const expectedCart = Cart({'123': 3});
       when(() => authRepository.currentUser).thenReturn(testUser); // null user
-      when(() => remoteCartRepository.fetchCart(testUser.uid))
-          .thenAnswer((_) => Future.value(initialCart));
-      when(() => remoteCartRepository.setCart(testUser.uid, expectedCart))
-          .thenAnswer((_) => Future.value());
+      when(() => remoteCartRepository.fetchCart(testUser.uid)).thenAnswer((_) => Future.value(initialCart));
+      when(() => remoteCartRepository.setCart(testUser.uid, expectedCart)).thenAnswer((_) => Future.value());
       final cartService = makeCartService();
       // run
       await cartService.addItem(const Item(productId: '123', quantity: 1));
       // verify
       verifyNever(() => localCartRepository.setCart(any()));
-      verify(() => remoteCartRepository.setCart(testUser.uid, expectedCart))
-          .called(1);
+      verify(() => remoteCartRepository.setCart(testUser.uid, expectedCart)).called(1);
     });
   });
 
@@ -116,10 +106,8 @@ void main() {
       const initialCart = Cart({'123': 3, '456': 1});
       const expectedCart = Cart({'456': 1});
       when(() => authRepository.currentUser).thenReturn(null); // null user
-      when(localCartRepository.fetchCart)
-          .thenAnswer((_) => Future.value(initialCart));
-      when(() => localCartRepository.setCart(expectedCart))
-          .thenAnswer((_) => Future.value());
+      when(localCartRepository.fetchCart).thenAnswer((_) => Future.value(initialCart));
+      when(() => localCartRepository.setCart(expectedCart)).thenAnswer((_) => Future.value());
       final cartService = makeCartService();
       // run
       await cartService.removeItemById('123');
@@ -133,17 +121,14 @@ void main() {
       const initialCart = Cart({'123': 3, '456': 1});
       const expectedCart = Cart({'456': 1});
       when(() => authRepository.currentUser).thenReturn(testUser); // null user
-      when(() => remoteCartRepository.fetchCart(testUser.uid))
-          .thenAnswer((_) => Future.value(initialCart));
-      when(() => remoteCartRepository.setCart(testUser.uid, expectedCart))
-          .thenAnswer((_) => Future.value());
+      when(() => remoteCartRepository.fetchCart(testUser.uid)).thenAnswer((_) => Future.value(initialCart));
+      when(() => remoteCartRepository.setCart(testUser.uid, expectedCart)).thenAnswer((_) => Future.value());
       final cartService = makeCartService();
       // run
       await cartService.removeItemById('123');
       // verify
       verifyNever(() => localCartRepository.setCart(any()));
-      verify(() => remoteCartRepository.setCart(testUser.uid, expectedCart))
-          .called(1);
+      verify(() => remoteCartRepository.setCart(testUser.uid, expectedCart)).called(1);
     });
   });
 }
---
+++ ecommerce_app/test/src/features/cart/application/cart_total_provider_test.dart
@@ -0,0 +1,76 @@
+import 'package:ecommerce_app/src/constants/test_products.dart';
+import 'package:ecommerce_app/src/features/cart/application/cart_service.dart';
+import 'package:ecommerce_app/src/features/cart/domain/cart.dart';
+import 'package:ecommerce_app/src/features/products/data/products_fake_repository.dart';
+import 'package:ecommerce_app/src/features/products/domain/product.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('cartTotalProvider', () {
+    ProviderContainer makeProviderContainer({
+      required Stream<Cart> cart,
+      required Stream<List<Product>> productsList,
+    }) {
+      final container = ProviderContainer(overrides: [
+        cartProvider.overrideWith((ref) => cart),
+        productsSreamProvider.overrideWith((ref) => productsList),
+      ]);
+      addTearDown(container.dispose);
+      return container;
+    }
+
+    test('loading cart', () async {
+      final container = makeProviderContainer(
+        cart: const Stream.empty(),
+        productsList: Stream.value(kTestProducts),
+      );
+      await container.read(productsSreamProvider.future);
+      final total = container.read(cartTotalPriceProvider);
+      expect(total, 0);
+    });
+
+    test('Empty cart', () async {
+      final container = makeProviderContainer(
+        cart: Stream.value(const Cart()),
+        productsList: Stream.value(kTestProducts),
+      );
+      await container.read(cartProvider.future);
+      await container.read(productsSreamProvider.future);
+      final total = container.read(cartTotalPriceProvider);
+      expect(total, 0);
+    });
+
+    test('one product with quantity = 1', () async {
+      final container = makeProviderContainer(
+        cart: Stream.value(const Cart({'1': 1})),
+        productsList: Stream.value(kTestProducts),
+      );
+      await container.read(cartProvider.future);
+      await container.read(productsSreamProvider.future);
+      final total = container.read(cartTotalPriceProvider);
+      expect(total, 15);
+    });
+
+    test('one product with quantity = 5', () async {
+      final container = makeProviderContainer(
+        cart: Stream.value(const Cart({'1': 5})),
+        productsList: Stream.value(kTestProducts),
+      );
+      await container.read(cartProvider.future);
+      await container.read(productsSreamProvider.future);
+      final total = container.read(cartTotalPriceProvider);
+      expect(total, 75); // 15 * 5 = 75
+    });
+
+    test('product not found', () async {
+      final container = makeProviderContainer(
+        cart: Stream.value(const Cart({'100': 1})),
+        productsList: Stream.value(kTestProducts),
+      );
+      await container.read(cartProvider.future);
+      await container.read(productsSreamProvider.future);
+      expect(() => container.read(cartTotalPriceProvider), throwsStateError);
+    });
+  });
+}
