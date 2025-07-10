5388b6a17d4faa5c4d4d9b63b588d3feb96a14d4
GabrielSantiago <marcosgabrielss23@gmail.com>
2025-07-10 16:20:11 -0300

    Implementing sync logic

--- ecommerce_app/lib/src/features/cart/application/cart_sync_service.dart
+++ ecommerce_app/lib/src/features/cart/application/cart_sync_service.dart
@@ -1,6 +1,10 @@
 import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
 import 'package:ecommerce_app/src/features/authentication/domain/app_user.dart';
-import 'package:flutter/foundation.dart';
+import 'package:ecommerce_app/src/features/cart/application/cart_service.dart';
+import 'package:ecommerce_app/src/features/cart/data/local/local_cart_repository.dart';
+import 'package:ecommerce_app/src/features/cart/data/remote/remote_cart_repository.dart';
+import 'package:ecommerce_app/src/features/cart/domain/cart.dart';
+import 'package:ecommerce_app/src/features/cart/domain/mutable_cart.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class CartSyncService {
@@ -14,9 +18,26 @@ class CartSyncService {
       final previousUser = previous?.value;
       final user = next.value;
       if (previousUser == null && user != null) {
-        debugPrint('signed in: ${user.uid}');
+        _moveItemsToRemoteCart(user.uid);
       }
     });
+  }
+
+  void _moveItemsToRemoteCart(String uid) async {
+    final localCartRepository = ref.watch(localCartRepositoryProvider);
+    final localCart = await localCartRepository.fetchCart();
+    if (localCart.toItemsList().isNotEmpty) {
+      // Get the remote cart data
+      final remoteCartRepository = ref.watch(remoteCartRepositoryProvider);
+      final remoteCart = await remoteCartRepository.fetchCart(uid);
+      // Move local items to the remote cart
+      final localItemsToAdd = localCart.toItemsList();
+      final updatedCart = remoteCart.addItems(localItemsToAdd);
+      // Write the updated local cart to the remote repository
+      await remoteCartRepository.setCart(uid, updatedCart);
+      // Remove all itens from the local cart
+      await localCartRepository.setCart(const Cart());
+    }
   }
 }
 
