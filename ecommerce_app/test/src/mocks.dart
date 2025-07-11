f02c73e38d4993a4771f4a42c861a1be00aae4c9
GabrielSantiago <marcosgabrielss23@gmail.com>
2025-07-11 17:51:13 -0300

    Mocking cart service

--- ecommerce_app/test/src/mocks.dart
+++ ecommerce_app/test/src/mocks.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce_app/src/features/authentication/data/auth_repository.dart';
+import 'package:ecommerce_app/src/features/cart/application/cart_service.dart';
 import 'package:ecommerce_app/src/features/cart/data/local/local_cart_repository.dart';
 import 'package:ecommerce_app/src/features/cart/data/remote/remote_cart_repository.dart';
 import 'package:ecommerce_app/src/features/products/data/products_fake_repository.dart';
@@ -11,3 +12,5 @@ class MockRemoteCartRepository extends Mock implements RemoteCartRepository {}
 class MockLocalCartRepository extends Mock implements LocalCartRepository {}
 
 class MockProductsRepository extends Mock implements ProductsFakeRepository {}
+
+class MockCartService extends Mock implements CartService {}
