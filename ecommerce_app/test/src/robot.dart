4d56fb758d1b7fcb5388858cf7a499f8b227fe5b
GabrielSantiago <marcosgabrielss23@gmail.com>
2025-07-11 17:40:13 -0300

    Cart robot

--- ecommerce_app/test/src/robot.dart
+++ ecommerce_app/test/src/robot.dart
@@ -1,29 +1,48 @@
 import 'package:ecommerce_app/src/app.dart';
 import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
+import 'package:ecommerce_app/src/features/cart/application/cart_sync_service.dart';
+import 'package:ecommerce_app/src/features/cart/data/local/fake_local_cart_repository.dart';
+import 'package:ecommerce_app/src/features/cart/data/local/local_cart_repository.dart';
+import 'package:ecommerce_app/src/features/cart/data/remote/fake_remote_cart_repository.dart';
+import 'package:ecommerce_app/src/features/cart/data/remote/remote_cart_repository.dart';
 import 'package:ecommerce_app/src/features/products/data/products_fake_repository.dart';
 import 'package:ecommerce_app/src/features/products/presentation/home_app_bar/more_menu_button.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'features/authentication/auth_robot.dart';
+import 'features/cart/cart_robot.dart';
 import 'features/products/products_robot.dart';
 
 class Robot {
   Robot(this.tester)
       : authRobot = AuthRobot(tester),
-        products = ProductsRobot(tester);
+        products = ProductsRobot(tester),
+        cart = CartRobot(tester);
   final WidgetTester tester;
   final AuthRobot authRobot;
   final ProductsRobot products;
+  final CartRobot cart;
 
   Future<void> pumpMyApp() async {
     final authRepository = FakeAuthRepository(addDelay: false);
     final productsRepository = ProductsFakeRepository(addDelay: false);
-    await tester.pumpWidget(ProviderScope(
+    final localCartRepository = FakeLocalCartRepository(addDelay: false);
+    final remoteCartRepository = FakeRemoteCartRepository(addDelay: false);
+
+    final container = ProviderContainer(
       overrides: [
-        authRepositoryProvider.overrideWithValue(authRepository),
         productsRepositoryProvider.overrideWithValue(productsRepository),
+        authRepositoryProvider.overrideWithValue(authRepository),
+        localCartRepositoryProvider.overrideWithValue(localCartRepository),
+        remoteCartRepositoryProvider.overrideWithValue(remoteCartRepository),
       ],
+    );
+    // * Initialize CartSyncService to start the listener
+    container.read(cartSyncServiceProvider);
+    // * Entry point of the app
+    await tester.pumpWidget(UncontrolledProviderScope(
+      container: container,
       child: const MyApp(),
     ));
     // Pump until there's no more frames to render
