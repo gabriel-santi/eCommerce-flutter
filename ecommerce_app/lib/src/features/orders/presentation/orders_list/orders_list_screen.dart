defd66e687c1898f80781579804b50b4271e2d36
GabrielSantiago <marcosgabrielss23@gmail.com>
2025-07-19 23:11:59 -0300

    Connecting screen with orders provider

--- ecommerce_app/lib/src/features/orders/presentation/orders_list/orders_list_screen.dart
+++ ecommerce_app/lib/src/features/orders/presentation/orders_list/orders_list_screen.dart
@@ -1,58 +1,52 @@
+import 'package:ecommerce_app/src/common_widgets/async_value_widget.dart';
+import 'package:ecommerce_app/src/features/orders/application/user_orders_provider.dart';
 import 'package:ecommerce_app/src/features/orders/presentation/orders_list/order_card.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
 import 'package:flutter/material.dart';
 import 'package:ecommerce_app/src/common_widgets/responsive_center.dart';
 import 'package:ecommerce_app/src/constants/app_sizes.dart';
-import 'package:ecommerce_app/src/features/orders/domain/order.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 /// Shows the list of orders placed by the signed-in user.
-class OrdersListScreen extends StatelessWidget {
+class OrdersListScreen extends ConsumerWidget {
   const OrdersListScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    // TODO: Read from data source
-    final orders = [
-      Order(
-        id: 'abc',
-        userId: '123',
-        items: {
-          '1': 1,
-          '2': 2,
-          '3': 3,
-        },
-        orderStatus: OrderStatus.confirmed,
-        orderDate: DateTime.now(),
-        total: 104,
-      ),
-    ];
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Your Orders'.hardcoded),
       ),
-      body: orders.isEmpty
-          ? Center(
-              child: Text(
-                'No previous orders'.hardcoded,
-                style: Theme.of(context).textTheme.displaySmall,
-                textAlign: TextAlign.center,
-              ),
-            )
-          : CustomScrollView(
-              slivers: <Widget>[
-                SliverList(
-                  delegate: SliverChildBuilderDelegate(
-                    (BuildContext context, int index) => ResponsiveCenter(
-                      padding: const EdgeInsets.all(Sizes.p8),
-                      child: OrderCard(
-                        order: orders[index],
+      body: Consumer(builder: (context, ref, _) {
+        final orders = ref.watch(userOrdersProvider);
+
+        return AsyncValueWidget(
+          value: orders,
+          onData: (orders) => orders.isEmpty
+              ? Center(
+                  child: Text(
+                    'No previous orders'.hardcoded,
+                    style: Theme.of(context).textTheme.displaySmall,
+                    textAlign: TextAlign.center,
+                  ),
+                )
+              : CustomScrollView(
+                  slivers: <Widget>[
+                    SliverList(
+                      delegate: SliverChildBuilderDelegate(
+                        (BuildContext context, int index) => ResponsiveCenter(
+                          padding: const EdgeInsets.all(Sizes.p8),
+                          child: OrderCard(
+                            order: orders[index],
+                          ),
+                        ),
+                        childCount: orders.length,
                       ),
                     ),
-                    childCount: orders.length,
-                  ),
+                  ],
                 ),
-              ],
-            ),
+        );
+      }),
     );
   }
 }
