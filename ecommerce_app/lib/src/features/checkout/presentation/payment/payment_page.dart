3cb82d23857060847e8c8f7e39d77aded2b5024f
GabrielSantiago <marcosgabrielss23@gmail.com>
2025-08-01 23:01:00 -0300

    Redirect on payment

--- ecommerce_app/lib/src/features/checkout/presentation/payment/payment_page.dart
+++ ecommerce_app/lib/src/features/checkout/presentation/payment/payment_page.dart
@@ -4,8 +4,10 @@ import 'package:ecommerce_app/src/features/cart/domain/cart.dart';
 import 'package:ecommerce_app/src/features/cart/presentation/shopping_cart/shopping_cart_item.dart';
 import 'package:ecommerce_app/src/features/cart/presentation/shopping_cart/shopping_cart_items_builder.dart';
 import 'package:ecommerce_app/src/features/checkout/presentation/payment/payment_button.dart';
+import 'package:ecommerce_app/src/routing/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 /// Payment screen showing the items in the cart (with read-only quantities) and
 /// a button to checkout.
@@ -14,8 +16,13 @@ class PaymentPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final cart = ref.watch(cartProvider);
+    ref.listen(cartTotalPriceProvider, (_, cartTotal) {
+      if (cartTotal == 0.0) {
+        context.goNamed(AppRoute.orders.name);
+      }
+    });
 
+    final cart = ref.watch(cartProvider);
     return AsyncValueWidget<Cart>(
         value: cart,
         onData: (cart) {
