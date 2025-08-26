5bb26152eae3e46fb62921d81eb1c13db593dc19
GabrielSantiago <marcosgabrielss23@gmail.com>
2025-08-25 23:15:30 -0300

    Remove unused import

--- ecommerce_app/lib/src/features/authentication/data/fake_auth_repository.dart
+++ ecommerce_app/lib/src/features/authentication/data/fake_auth_repository.dart
@@ -2,7 +2,6 @@ import 'package:ecommerce_app/src/exceptions/app_exceptions.dart';
 import 'package:ecommerce_app/src/features/authentication/domain/app_user.dart';
 import 'package:ecommerce_app/src/features/authentication/data/auth_repository.dart';
 import 'package:ecommerce_app/src/features/authentication/domain/fake_app_user.dart';
-import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
 import 'package:ecommerce_app/src/utils/delay.dart';
 import 'package:ecommerce_app/src/utils/in_memory_store.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
