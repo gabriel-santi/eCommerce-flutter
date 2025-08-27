9d7b0b55c572c579c10cf4469d9e4f1dfebb1c41
GabrielSantiago <marcosgabrielss23@gmail.com>
2025-08-26 23:51:55 -0300

    Using error logger on async errors

--- ecommerce_app/lib/src/exceptions/error_logger.dart
+++ ecommerce_app/lib/src/exceptions/error_logger.dart
@@ -1,9 +1,16 @@
+import 'package:ecommerce_app/src/exceptions/app_exceptions.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class ErrorLogger {
   void logError(Object error, StackTrace? stack) {
+    // * This can be replaced with a call to a crash reporting tool of choice
     debugPrint('$error, $stack');
+  }
+
+  void logAppException(AppException exception) {
+    // * This can be replaced with a call to a crash reporting tool of choice
+    debugPrint('$exception');
   }
 }
 
