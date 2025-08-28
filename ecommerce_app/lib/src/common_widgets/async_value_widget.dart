e4a33b9fdea9da14235bc0b417e1a091c5aa3d77
GabrielSantiago <marcosgabrielss23@gmail.com>
2025-08-27 21:31:03 -0300

    Sliver async value widget

--- ecommerce_app/lib/src/common_widgets/async_value_widget.dart
+++ ecommerce_app/lib/src/common_widgets/async_value_widget.dart
@@ -3,8 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class AsyncValueWidget<T> extends StatelessWidget {
-  const AsyncValueWidget(
-      {super.key, required this.value, required this.onData, this.onLoading});
+  const AsyncValueWidget({super.key, required this.value, required this.onData, this.onLoading});
   final AsyncValue<T> value;
   final Widget Function(T) onData;
   final Widget Function()? onLoading;
@@ -14,7 +13,24 @@ class AsyncValueWidget<T> extends StatelessWidget {
     return value.when(
         data: onData,
         error: (e, st) => Center(child: ErrorMessageWidget(e.toString())),
-        loading: () =>
-            onLoading?.call() ?? Center(child: CircularProgressIndicator()));
+        loading: () => onLoading?.call() ?? Center(child: CircularProgressIndicator()));
+  }
+}
+
+/// Sliver equivalent of [AsyncValueWidget]
+class AsyncValueSliverWidget<T> extends StatelessWidget {
+  const AsyncValueSliverWidget({super.key, required this.value, required this.data});
+  final AsyncValue<T> value;
+  final Widget Function(T) data;
+
+  @override
+  Widget build(BuildContext context) {
+    return value.when(
+      data: data,
+      loading: () => const SliverToBoxAdapter(child: Center(child: CircularProgressIndicator())),
+      error: (e, st) => SliverToBoxAdapter(
+        child: Center(child: ErrorMessageWidget(e.toString())),
+      ),
+    );
   }
 }
