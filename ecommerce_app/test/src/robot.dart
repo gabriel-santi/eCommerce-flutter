0f6cc1c45e86259c00cbe81b910c5ebe1f0edb53
GabrielSantiago <marcosgabrielss23@gmail.com>
2025-09-09 23:16:03 -0300

    Updating test

--- ecommerce_app/test/src/robot.dart
+++ ecommerce_app/test/src/robot.dart
@@ -5,8 +5,10 @@ import 'package:ecommerce_app/src/features/cart/data/local/fake_local_cart_repos
 import 'package:ecommerce_app/src/features/cart/data/local/local_cart_repository.dart';
 import 'package:ecommerce_app/src/features/cart/data/remote/fake_remote_cart_repository.dart';
 import 'package:ecommerce_app/src/features/cart/data/remote/remote_cart_repository.dart';
+import 'package:ecommerce_app/src/features/orders/data/fake_orders_repository.dart';
 import 'package:ecommerce_app/src/features/products/data/products_fake_repository.dart';
 import 'package:ecommerce_app/src/features/products/presentation/home_app_bar/more_menu_button.dart';
+import 'package:ecommerce_app/src/features/reviews/data/fake_reviews_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -15,6 +17,7 @@ import 'features/cart/cart_robot.dart';
 import 'features/checkout/checkout_robot.dart';
 import 'features/orders/orders_robot.dart';
 import 'features/products/products_robot.dart';
+import 'features/reviews/reviews_robot.dart';
 
 class Robot {
   Robot(this.tester)
@@ -22,19 +25,23 @@ class Robot {
         products = ProductsRobot(tester),
         cart = CartRobot(tester),
         checkout = CheckoutRobot(tester),
-        orders = OrdersRobot(tester);
+        orders = OrdersRobot(tester),
+        reviews = ReviewsRobot(tester);
   final WidgetTester tester;
   final AuthRobot auth;
   final ProductsRobot products;
   final CartRobot cart;
   final CheckoutRobot checkout;
   final OrdersRobot orders;
+  final ReviewsRobot reviews;
 
   Future<void> pumpMyApp() async {
     final authRepository = FakeAuthRepository(addDelay: false);
     final productsRepository = ProductsFakeRepository(addDelay: false);
     final localCartRepository = FakeLocalCartRepository(addDelay: false);
     final remoteCartRepository = FakeRemoteCartRepository(addDelay: false);
+    final ordersRepository = FakeOrdersRepository(addDelay: false);
+    final reviewsRepository = FakeReviewsRepository(addDelay: false);
 
     final container = ProviderContainer(
       overrides: [
@@ -42,6 +49,8 @@ class Robot {
         authRepositoryProvider.overrideWithValue(authRepository),
         localCartRepositoryProvider.overrideWithValue(localCartRepository),
         remoteCartRepositoryProvider.overrideWithValue(remoteCartRepository),
+        ordersRepositoryProvider.overrideWithValue(ordersRepository),
+        reviewsRepositoryProvider.overrideWithValue(reviewsRepository),
       ],
     );
     // * Initialize CartSyncService to start the listener
