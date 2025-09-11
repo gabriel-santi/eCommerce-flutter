f8fb7689f6e450657284f637822eae58a207120a
GabrielSantiago <marcosgabrielss23@gmail.com>
2025-09-10 23:05:09 -0300

    Leave review controller

---
+++ ecommerce_app/lib/src/features/reviews/presentation/leave_review_screen/leave_review_controller.dart
@@ -0,0 +1,36 @@
+import 'package:ecommerce_app/src/features/products/domain/product.dart';
+import 'package:ecommerce_app/src/features/reviews/application/reviews_service.dart';
+import 'package:ecommerce_app/src/features/reviews/domain/review.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class LeaveReviewController extends StateNotifier<AsyncValue<void>> {
+  LeaveReviewController({
+    required this.reviewService,
+    required this.currentDateBuilder,
+  }) : super(AsyncData(null));
+
+  final ReviewsService reviewService;
+  final DateTime Function() currentDateBuilder;
+
+  Future<void> submitReview({
+    required ProductID productId,
+    required double rating,
+    required String comment,
+  }) async {
+    final review = Review(
+      rating: rating,
+      comment: comment,
+      date: currentDateBuilder(),
+    );
+    state = AsyncLoading();
+    state = await AsyncValue.guard(() => reviewService.submitReview(productId: productId, review: review));
+  }
+}
+
+final leaveReviewControllerProvider = StateNotifierProvider.autoDispose<LeaveReviewController, AsyncValue<void>>((ref) {
+  final reviewService = ref.watch(reviewsServiceProvider);
+  return LeaveReviewController(
+    reviewService: reviewService,
+    currentDateBuilder: () => DateTime.now(),
+  );
+});
